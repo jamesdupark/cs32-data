97e8b8143024e25fc7a451077570721f2b0940ce
Andrew Li <Andrewli2024@gmail.com>
2022-03-02 02:36:38 -0500

    feat: added packaging and documentation to the DBTables package

--- src/main/java/edu/brown/cs/student/main/DBParsing/DBPermissions/DBPerm.java
+++
@@ -1,7 +0,0 @@
-package edu.brown.cs.student.main.DBParsing.DBPermissions;
-
-import java.util.Map;
-
-public interface DBPerm {
-  Map<Integer, String> getIndexTable();
-}
--- src/main/java/edu/brown/cs/student/main/DBParsing/DBPermissions/DatabaseHoroscopePerm.java
+++
@@ -1,4 +0,0 @@
-package edu.brown.cs.student.main.DBParsing.DBPermissions;
-
-public class DatabaseHoroscopePerm {
-}
