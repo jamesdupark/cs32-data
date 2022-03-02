86ee0834188f1ca2fda7f31c32d7f64eac64e96b
Andrew Li <Andrewli2024@gmail.com>
2022-03-02 02:50:38 -0500

    feat: added packaging and documentation to the DBItems Package

--- src/main/java/edu/brown/cs/student/main/DBParsing/DBItems/DatabaseAnimal.java
+++ src/main/java/edu/brown/cs/student/main/DBParsing/DBItems/DatabaseAnimal.java
@@ -24,4 +24,73 @@ public class DatabaseAnimal {
     this.age = null;
     this.height = null;
   }
+
+  /**
+   * Accessor method for the id field of the animal.
+   * @return the id field of the animal
+   */
+  public Integer getId() {
+    return id;
+  }
+
+  /**
+   * Mutator method for the id field of the animal.
+   * @param id Integer representing the new id of the animal.
+   */
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  /**
+   * Accessor method for the animal type field of the animal.
+   * @return the animalType field of the animal.
+   */
+  public String getAnimalType() {
+    return animalType;
+  }
+
+  /**
+   * Mutator method for the animal type field of the animal.
+   * @param animalType String representing the new animal type of the animal.
+   */
+  public void setAnimalType(String animalType) {
+    this.animalType = animalType;
+  }
+
+  /**
+   * Accessor method for the age field of the animal.
+   * @return the age field of the animal.
+   */
+  public Integer getAge() {
+    return age;
+  }
+
+  /**
+   * Mutator method for the age field of the animal.
+   * @param age Integer representing the new age of the animal.
+   */
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  /**
+   * Accessor method for the height field of the animal.
+   * @return the height field of the animal.
+   */
+  public Double getHeight() {
+    return height;
+  }
+
+  /**
+   * Mutator method for the height field of the animal.
+   * @param height Integer representing the new height of the animal.
+   */
+  public void setHeight(Double height) {
+    this.height = height;
+  }
+  @Override
+  public String toString() {
+    return "DatabaseAnimal{" + "id=" + id + ", animalType='" + animalType
+        + '\'' + ", age=" + age + ", height=" + height + '}';
+  }
 }
