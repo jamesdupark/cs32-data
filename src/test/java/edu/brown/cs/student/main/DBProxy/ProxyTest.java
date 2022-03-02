56a26a8fa1bb488767b8b384780e82f8f2eec5ea
Andrew Li <Andrewli2024@gmail.com>
2022-03-02 16:37:10 -0500

    test: wrote unit tests for ProxyTest

--- src/test/java/edu/brown/cs/student/main/DBProxy/ProxyTest.java
+++ src/test/java/edu/brown/cs/student/main/DBProxy/ProxyTest.java
@@ -1,7 +1,10 @@
 package edu.brown.cs.student.main.DBProxy;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,27 +14,51 @@ import static org.junit.Assert.assertEquals;
  * Class to test the Proxy.
  */
 public class ProxyTest {
+  Map<String, String> studentTablePermissions;
+  /**
+   * Method to initialize and populate the three KDTrees fields.
+   * @throws IOException if the CSVReader is unable to read the file path
+   */
+  @Before
+  public void setup() throws IOException {
+    // initialize student table permissions
+    studentTablePermissions = new HashMap<>();
+    studentTablePermissions.put("names", "RW");
+    studentTablePermissions.put("traits", "RW");
+    studentTablePermissions.put("skills", "RW");
+    studentTablePermissions.put("interests", "RW");
+  }
+
   /**
    * Method to test setupCommandPermissions.
    */
   @Test
-  public void testSetupCommandPermissions() {
-    Map<String, String> tablePermissions = new HashMap<>();
-    tablePermissions.put("names", "RW");
-    tablePermissions.put("traits", "RW");
-    tablePermissions.put("skills", "RW");
-    tablePermissions.put("interests", "RW");
+  public void testSetupCommandPermissions() throws IOException {
+    setup();
+    Proxy proxy = new Proxy("data/recommendation/sql/data.sqlite3",
+        studentTablePermissions);
+    Map<String, String> sqlPermissions = proxy.getSqlPermissions();
+    assertEquals(sqlPermissions.size(), 8);
+    assertEquals(sqlPermissions.get("SELECT"), "R");
+    assertEquals(sqlPermissions.get("INSERT"), "R");
+    assertEquals(sqlPermissions.get("DROP"), "RW");
+    assertEquals(sqlPermissions.get("UPDATE"), "RW");
+    assertEquals(sqlPermissions.get("DELETE"), "RW");
+    assertEquals(sqlPermissions.get("ALTER"), "RW");
+    assertEquals(sqlPermissions.get("JOIN"), "R");
+    assertEquals(sqlPermissions.get("TRUNCATE"), "RW");
+  }
 
-//    Proxy proxy = new Proxy("../../data/recommendation/sql/data.sqlite3", tablePermissions);
-//    Map<String, String> sqlPermissions = proxy.getSqlPermissions();
-//    assertEquals(sqlPermissions.size(), 8);
-//    assertEquals(sqlPermissions.get("SELECT"), "R");
-//    assertEquals(sqlPermissions.get("INSERT"), "R");
-//    assertEquals(sqlPermissions.get("DROP"), "RW");
-//    assertEquals(sqlPermissions.get("UPDATE"), "RW");
-//    assertEquals(sqlPermissions.get("DELETE"), "RW");
-//    assertEquals(sqlPermissions.get("ALTER"), "RW");
-//    assertEquals(sqlPermissions.get("JOIN"), "R");
-//    assertEquals(sqlPermissions.get("TRUNCATE"), "RW");
+  /**
+   * Method to test connectDB.
+   */
+  @Test
+  public void testConnectDB() throws SQLException, IOException, ClassNotFoundException {
+    setup();
+    Proxy proxy = new Proxy("data/recommendation/sql/data.sqlite3",
+        studentTablePermissions);
+    assertEquals(proxy.getConn(), null);
+    proxy.connectDB();
+    assertEquals(proxy.getConn().toString(), "org.sqlite.jdbc4.JDBC4Connection@3ef41c66");
   }
 }
